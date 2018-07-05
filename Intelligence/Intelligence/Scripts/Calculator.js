752d36c344b0e64b8e2747ddd2e6c49a7f2547a6
Jeremy <jeremiahsuess@hotmail.com>
2018-07-05 07:26:59 -0400

    saving

---
+++ Intelligence/Intelligence/Scripts/Calculator.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Calculator;
+(function (Calculator) {
+    function bindEvents() {
+        calculate();
+    }
+    Calculator.bindEvents = bindEvents;
+    /**
+     * This calculator calculates compound interest when the user
+     * enters data into the fields
+     */
+    function calculate() {
+    }
+})(Calculator = exports.Calculator || (exports.Calculator = {}));
+//# sourceMappingURL=Calculator.js.map
---
+++ Intelligence/Intelligence/Scripts/Calculator.ts
@@ -0,0 +1,50 @@
+﻿export namespace Calculator {
+
+    export function bindEvents() {
+
+        calculate();
+
+
+
+    }
+
+
+    /**
+     * This calculator calculates compound interest when the user
+     * enters data into the fields
+     */
+    function calculate() {
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
