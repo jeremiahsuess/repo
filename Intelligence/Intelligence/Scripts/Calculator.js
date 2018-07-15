9ebf8da1b42a2532f9ebf685b06db848b74c50b6
Jeremy <jeremiahsuess@hotmail.com>
2018-07-15 18:04:57 -0400

    saving work

--- Intelligence/Intelligence/Scripts/Calculator.js
+++ Intelligence/Intelligence/Scripts/Calculator.js
@@ -3,15 +3,52 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Calculator;
 (function (Calculator) {
     function bindEvents() {
-        calculate();
         setupClickableLabels();
+        calculateValueEvent();
     }
     Calculator.bindEvents = bindEvents;
+    function calculateValueEvent() {
+        $("#CurrentPrincipal,#AnnualAddition,#YearsToGrow,#InterestRate,#AnnualCompoundingsAmount,#HasAdditionsAtStart ")
+            .on("input change", function () {
+            calculate();
+        });
+    }
     /**
      * This calculator calculates compound interest when the user
      * enters data into the fields
      */
     function calculate() {
+        var currentPrincipal = parseNumber($("#CurrentPrincipal"));
+        var annualAddition = parseNumber($("#AnnualAddition"));
+        var yearsToGrow = parseNumber($("#YearsToGrow"));
+        var interestRate = parseNumber($("#InterestRate"));
+        var annualCompoundingsAmount = parseNumber($("#AnnualCompoundingsAmount"));
+        var hasAdditionsAtStart = $("#HasAdditionsAtStart").val();
+        var futureValue = 0;
+        if (hasAdditionsAtStart) {
+            // Beginning of the period
+            // Balance(Y)   =   P(1 + r)Y   +   c[ ((1 + r)Y + 1 - (1 + r)) / r ]
+            //futureValue = (currentPrincipal * Math.pow((1 + interestRate), yearsToGrow)) +
+            //(annualAddition * (Math.pow((1 + interestRate), yearsToGrow) + 1) -
+            //    (annualAddition * (1 + interestRate)) / interestRate);
+            for (var i = 1; i <= 12; i++) {
+                futureValue = (futureValue + currentPrincipal) * (1 + interestRate);
+            }
+        }
+        else {
+            // End of the period:
+        }
+        console.log(futureValue);
+        $("#FutureValue").val(futureValue);
+    }
+    /**
+     * Parses a numeric value using the value from a Jquery selector.
+     * If the value is not a number, it will return 0.
+     * @param $selector
+     */
+    function parseNumber($selector) {
+        return isNaN($selector.val()) ? 0 : $selector.val();
+        //TODO fix this to PARSEDOUBLE
     }
     /**
      * When the "start" or "end" labels are clicked, it will set the
--- Intelligence/Intelligence/Scripts/Calculator.ts
+++ Intelligence/Intelligence/Scripts/Calculator.ts
@@ -1,22 +1,60 @@
 ﻿export namespace Calculator {
 
     export function bindEvents() {
-
-        calculate();
+        
         setupClickableLabels();
-
+        calculateValueEvent();
 
     }
 
+    function calculateValueEvent():void {
+        $("#CurrentPrincipal,#AnnualAddition,#YearsToGrow,#InterestRate,#AnnualCompoundingsAmount,#HasAdditionsAtStart ")
+            .on("input change",
+                () => {
+                    calculate();
+                });
+    }
 
     /**
      * This calculator calculates compound interest when the user
      * enters data into the fields
      */
-    function calculate() {
+    function calculate(): void {
+        var currentPrincipal: number = parseNumber($("#CurrentPrincipal"));
+        var annualAddition: number = parseNumber($("#AnnualAddition"));
+        var yearsToGrow: number = parseNumber($("#YearsToGrow"));
+        var interestRate: number = parseNumber($("#InterestRate"));
+        var annualCompoundingsAmount: number = parseNumber($("#AnnualCompoundingsAmount"));
+        var hasAdditionsAtStart: boolean = $("#HasAdditionsAtStart").val();
+        var futureValue: number = 0;
 
+        if (hasAdditionsAtStart) {
+            // Beginning of the period
+            // Balance(Y)   =   P(1 + r)Y   +   c[ ((1 + r)Y + 1 - (1 + r)) / r ]
 
+            //futureValue = (currentPrincipal * Math.pow((1 + interestRate), yearsToGrow)) +
+            //(annualAddition * (Math.pow((1 + interestRate), yearsToGrow) + 1) -
+            //    (annualAddition * (1 + interestRate)) / interestRate);
+            for (var i = 1; i <= 12; i++) {
+                futureValue = (futureValue + currentPrincipal) * (1 + interestRate);
+            }
 
+        } else {
+            // End of the period:
+
+        }
+        console.log(futureValue);
+        $("#FutureValue").val(futureValue);
+    }
+
+    /**
+     * Parses a numeric value using the value from a Jquery selector.
+     * If the value is not a number, it will return 0.
+     * @param $selector
+     */
+    function parseNumber($selector: JQuery): number {
+        return isNaN($selector.val()) ? 0 : $selector.val();
+        //TODO fix this to PARSEDOUBLE
     }
 
     /**
@@ -38,32 +76,4 @@
                 //$('#HasAdditionsAtStart').prop('checked', false);
             });
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
